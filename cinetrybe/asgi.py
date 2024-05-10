eee1be470db5481b263d87f11e02abb6c4069ef0
Thedywender <thedywender@hotmail.com>
2024-05-10 19:48:56 -0400

    building docker

--- cinetrybe/asgi.py
+++ cinetrybe/asgi.py
@@ -3,7 +3,7 @@ ASGI config for cinetrybe project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinetrybe.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cinetrybe.settings")
 
 application = get_asgi_application()
