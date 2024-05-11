0b333580b03096e1c3a80364451548c0913973d9
Thedywender <thedywender@hotmail.com>
2024-05-11 09:51:13 -0400

    testing

--- cinetrybe/asgi.py
+++ cinetrybe/asgi.py
@@ -3,7 +3,7 @@ ASGI config for cinetrybe project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
